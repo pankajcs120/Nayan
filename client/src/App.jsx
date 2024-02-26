ed7e1c91133f87e2aff5f31a5deecaf46fe325e8
pankajcs120 <128893521+pankajcs120@users.noreply.github.com>
2024-02-26 14:38:05 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
   const [message,setMessage]=useState("");
 
   useEffect(()=>{
-    fetch("https://nayan-backend.onrender.com/api").then(res=>res.json()).then(data=>setMessage(data.message));
+    fetch("https://nayan-backend.onrender.com").then(res=>res.json()).then(data=>setMessage(data.message));
   })
 
   const Layout = () => {
